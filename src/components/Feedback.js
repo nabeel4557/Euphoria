7292d7ad1416d6c1c8d246222708735f3bbd00f9
nabeel4557 <143259191+nabeel4557@users.noreply.github.com>
2024-09-20 19:38:08 +0530

    updated

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,62 +1,102 @@
-import React from 'react'
 
+
+import React from 'react';
 import { IoStar, IoStarHalf, IoStarOutline } from "react-icons/io5";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 
 function Feedback() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1280, 
+                settings: {
+                    slidesToShow: 2, 
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1, 
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    };
+
     return (
         <div className='wrapper py-10'>
             <div className='flex items-center gap-x-4 mb-[50px]'>
-                <div className='bg-[#8A33FD] w-[6px] h-[30px]  '></div>
+                 <div className='bg-[#8A33FD] w-[6px] h-[30px]  '></div>
                 <h3 className='text-[28px] font-bold'>Feedback</h3>
             </div>
-            <div className='flex font-causten w-full space-x-8'>
-                <div className='w-1/3 border border-[#BEBCBD] p-6 rounded-lg'>
-                    <div className='flex justify-between'>
-                        <div>
-                            <img src='/assets/span-1.svg' alt="User" />
-                            <h4 className='text-[#3C4242] text-[22px] font-semibold mt-3 mb-5'>Floyd Miles</h4>
+            <Slider {...settings}>
+                <div className='pr-5 h-[300px] max-[430px]:h-[370px]'>
+                    <div className=' border border-[#BEBCBD] p-6 rounded-lg h-full'>
+                        <div className='flex justify-between'>
+                            <div>
+                                <div className='w-[50px] h-[50px]'>
+                                    <img src='/assets/span-1.svg' alt="User" className='w-full h-full'/>
+                                </div>
+                                <h4 className='text-[#3C4242] text-[22px] font-semibold mt-3 mb-5'>Floyd Miles</h4>
+                            </div>
+                            <div className='flex space-x-2 text-[#EDD146]'>
+                                <IoStar /><IoStar /><IoStar /><IoStarHalf /><IoStarOutline />
+                            </div>
                         </div>
-                        <div className='flex space-x-2 text-[#EDD146]'>
-                            <IoStar /><IoStar /><IoStar /><IoStarHalf /><IoStarOutline />
+                        <div className='max-w-[95%]'>
+                            <p className='text-[14px] text-[#807D7E] leading-[16px] font-normal'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p> 
+                            <p className='text-[14px] text-[#807D7E] font-normal leading-[16px]'>Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
                         </div>
-                    </div>
-                    <div className='max-w-[95%]'>
-                        <p className='text-[14px] text-[#807D7E] leading-[16px] font-normal'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p> 
-                        <p className='text-[14px] text-[#807D7E] font-normal leading-[16px]'>Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
                     </div>
                 </div>
-                <div className='w-1/3 border border-[#BEBCBD] p-6 rounded-lg'>
-                    <div className='flex justify-between'>
-                        <div>
-                            <img src='/assets/span-2.svg
-                            ' alt="User" />
-                            <h4 className='text-[#3C4242] text-[22px] font-semibold mt-3 mb-5'>Ronald Richards</h4>
+                <div className='pr-5 h-[300px] max-[430px]:h-[370px]'>
+                    <div className=' border border-[#BEBCBD] p-6 rounded-lg h-full'>
+                        <div className='flex justify-between'>
+                            <div>
+                                <div className='w-[50px] h-[50px]'>
+                                    <img src='/assets/span-2.svg' alt="User" className='w-full h-full'/>
+                                </div>
+                                <h4 className='text-[#3C4242] text-[22px] font-semibold mt-3 mb-5'>Ronald Richards</h4>
+                            </div>
+                            <div className='flex space-x-2 text-[#EDD146]'>
+                                <IoStar /><IoStar /><IoStar /><IoStar /><IoStarOutline />
+                            </div>
                         </div>
-                        <div className='flex space-x-2 text-[#EDD146]'>
-                            <IoStar /><IoStar /><IoStar /><IoStar /><IoStarOutline />
+                        <div className='max-w-[95%]'>
+                            <p className='text-[14px] text-[#807D7E] leading-[16px] font-normal'>ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p> 
                         </div>
-                    </div>
-                    <div className='max-w-[95%]'>
-                        <p className='text-[14px] text-[#807D7E] leading-[16px] font-normal'>ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p> 
                     </div>
                 </div>
-                <div className='w-1/3 border border-[#BEBCBD] p-6 rounded-lg'>
-                    <div className='flex justify-between'>
-                        <div>
-                            <img src='/assets/span-3.svg' alt="User" />
-                            <h4 className='text-[#3C4242] text-[22px] font-semibold mt-3 mb-5'>Savannah Nguyen</h4>
+                <div className='pr-5 h-[300px] max-[430px]:h-[370px]'>
+                    <div className=' border border-[#BEBCBD] p-6 rounded-lg h-full'>
+                        <div className='flex justify-between'>
+                            <div>
+                                <div className='w-[50px] h-[50px]'>
+                                    <img src='/assets/span-3.svg' alt="User" className='w-full h-full'/>
+                                </div>
+                                <h4 className='text-[#3C4242] text-[22px] font-semibold mt-3 mb-5'>Savannah Nguyen</h4>
+                            </div>
+                            <div className='flex space-x-2 text-[#EDD146]'>
+                                <IoStar /><IoStar /><IoStar /><IoStarHalf /><IoStarOutline />
+                            </div>
                         </div>
-                        <div className='flex space-x-2 text-[#EDD146]'>
-                            <IoStar /><IoStar /><IoStar /><IoStarHalf /><IoStarOutline />
+                        <div className='max-w-[95%]'>
+                            <p className='text-[14px] text-[#807D7E] leading-[16px] font-normal'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p> 
+                            <p className='text-[14px] text-[#807D7E] font-normal leading-[16px]'>Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
                         </div>
-                    </div>
-                    <div className='max-w-[95%]'>
-                        <p className='text-[14px] text-[#807D7E] leading-[16px] font-normal'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p> 
-                        <p className='text-[14px] text-[#807D7E] font-normal leading-[16px]'>Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
                     </div>
                 </div>
-            </div>
+            </Slider>
         </div>
     )
 }
--- src/components/Women.js
+++ src/components/Women.js
@@ -11,14 +11,14 @@ function Women() {
         <div className='bg-[#8A33FD] w-[6px] h-[30px]'></div>
         <h3 className='text-[28px] font-bold'>Categories For Women</h3>
       </div>
-            <div className='grid grid-cols-4 w-full max-[768px]:grid-cols-2'>
+            <div className='grid grid-cols-4 w-full max-[768px]:grid-cols-2 max-[480px]:grid-cols-1'>
               {women.map((item)=> (
                 <Link to={`/products/${item.id}`}>
                     <div className='mb-10 '>
-                        <div className='w-[271px] h-[393px] max-[1280px]:w-[260px] max-[1280px]:h-[375px] max-[1080px]:w-[200px] max-[1080px]:h-[300px] max-[980px]:w-[150px] max-[980px]:h-[200px] max-[768px]:w-[250px] max-[768px]:h-[300px] max-[640px]:w-[200px]'>
+                        <div className='w-[271px] h-[393px] max-[1280px]:w-[260px] max-[1280px]:h-[375px] max-[1080px]:w-[200px] max-[1080px]:h-[300px] max-[980px]:w-[150px] max-[980px]:h-[200px] max-[768px]:w-[250px] max-[768px]:h-[300px] max-[640px]:w-[200px] max-[480px]:w-full max-[480px]:h-full'>
                         <img className='w-[100%] h-[100%]' src={item.image} alt={item.name}/>
                         </div>
-                        <div className='flex justify-between items-center w-[271px] max-[1280px]:w-[260px] max-[1080px]:w-[200px] max-[980px]:w-[150px] max-[768px]:w-[250px] max-[640px]:w-[200px]'>
+                        <div className='flex justify-between items-center w-[271px] max-[1280px]:w-[260px] max-[1080px]:w-[200px] max-[980px]:w-[150px] max-[768px]:w-[250px] max-[640px]:w-[200px] max-[480px]:w-full'>
                         <div className='mt-[20px]'>
                             <h5 className=' text-[18px] font-bold max-[1080px]:text-[15px] max-[980px]:text-[11px] max-[768px]:text-[16px]'>{item.name}</h5>
                             <span className=' text-[14px] max-[980px]:text-[11px] max-[768px]:text-[14px] text-[#7F7F7F]'>Explore Now!</span>
